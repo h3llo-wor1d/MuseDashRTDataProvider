6ba735384c268e855a4b9e039ae65768dabb6b04
willow-rubenstein <contact.notashe@gmail.com>
2022-08-09 14:53:28 -0400

    Finished and working! Connect via localhost:8080 for a realtime feed of score, combo, and more coming soon!

--- Main.cs
+++ Main.cs
@@ -9,20 +9,33 @@ using Assets.Scripts.GameCore.HostComponent;
 using Assets.Scripts.PeroTools.Commons;
 using GameLogic;
 using HarmonyLib;
+using UnityEngine.AddressableAssets;
 
 namespace MuseDashRTDataProvider
 {
 
     [HarmonyPatch(typeof(BattleEnemyManager), "SetPlayResult")]
-    public class SetPlayPatch
+    internal class SetPlayPatch
     {
-        public void Postfix(int idx, byte result, bool isMulStart = false, bool isMulEnd = false, bool isLeft = false)
+        private static void Postfix(int idx, byte result, bool isMulStart = false, bool isMulEnd = false, bool isLeft = false)
         {
-            Console.WriteLine($"Switching for result {result}");
-            switch (result)
+            string key = "";
+            switch ((int)result)
             {
-
+                case 1:
+                    key = "hitMiss";
+                    break;
+                case 3:
+                    key = "hitGreat";
+                    break;
+                case 4:
+                    key = "hitPerfect";
+                    break;
             }
+            if (key != "")
+            {
+                Main.handleNewScore(key);
+            }     
         }
     }
 
@@ -33,11 +46,32 @@ namespace MuseDashRTDataProvider
         public static websocketServer server = new websocketServer();
         public Task serverInstance = null;
 
+        public static void addToKey(string key)
+        {
+            int cur = (int)gameFeed[key];
+            gameFeed[key] = cur + 1;
+        }
+
+        public static void handleNewScore(string key)
+        {
+            if (key == "hitMiss")
+            {
+                gameFeed["comboCount"] = 0;
+            } else
+            {
+                addToKey("comboCount");
+            }
+            Console.WriteLine($"Debug: handling new score for key {key}");
+            addToKey(key);
+            clientUpdate();
+        }
+
         public static void clientUpdate()
         {
             // Basically just copied and pasted from my Celeste mod lol
             try
             {
+                Console.WriteLine($"Debug: sending message with payload {JsonConvert.SerializeObject(gameFeed)}");
                 server.sendMessage(JsonConvert.SerializeObject(gameFeed));
             }
             catch (Exception ex)
@@ -46,34 +80,13 @@ namespace MuseDashRTDataProvider
             }
         }
 
-        public void addListing(int id, string name)
-        {
-            List<int> list = (List<int>)gameFeed[name];
-            list.Add(id);
-            gameFeed[name] = list;
-        }
-
         private void resetFeed()
         {
             // Init all variables
-            gameFeed["collectableNoteMiss"] = 0;
-            gameFeed["ghostMiss"] = 0;
-            gameFeed["greatNum"] = 0;
-        }
-
-        public void removeListing(int id, string name)
-        {
-            try
-            {
-                List<int> list = (List<int>)gameFeed[name];
-                list.Remove(id);
-                gameFeed[name] = list;
-            }
-            catch
-            {
-                LoggerInstance.Msg("err: removeListing failed. Continuing...");
-            }
-
+            gameFeed["hitMiss"] = 0;
+            gameFeed["hitGreat"] = 0;
+            gameFeed["hitPerfect"] = 0;
+            gameFeed["comboCount"] = 0;
         }
 
         public override void OnApplicationStart()
@@ -81,6 +94,16 @@ namespace MuseDashRTDataProvider
             resetFeed();
             serverInstance = Task.Run(() => server.startServer(8080));
             LoggerInstance.Msg("MuseDashRTDataProvider v0.0.1 started...");
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            if (sceneName == "GameMain") {}
+            else
+            {
+                resetFeed();
+                clientUpdate();
+            }
         }
 
     }
